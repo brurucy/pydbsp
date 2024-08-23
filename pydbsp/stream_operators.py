a0a9ddf04857328b9c5439ec81863e54e51ce2d9
brurucy <brurucy@gmail.com>
2024-08-23 21:51:57 +0300

    Added the `Incremental2` operator

--- pydbsp/stream_operators.py
+++ pydbsp/stream_operators.py
@@ -128,6 +128,66 @@ class Integrate(UnaryOperator[T, T]):
         return self.delayed_stream.step()
 
 
+class Incremental2(BinaryOperator[T, R, S]):
+    integrated_stream_a: Integrate[T]
+    delayed_integrated_stream_a: Delay[T]
+
+    integrated_stream_b: Integrate[R]
+    delayed_integrated_stream_b: Delay[R]
+
+    f2_a_b: Lifted2[T, R, S]
+    f2_delayed_integrated_a_b: Lifted2[T, R, S]
+    f2_a_delayed_integrated_b: Lifted2[T, R, S]
+
+    def __init__(
+        self,
+        stream_a: Optional[StreamHandle[T]],
+        stream_b: Optional[StreamHandle[R]],
+        f2: F2[T, R, S],
+        output_stream_group: Optional[AbelianGroupOperation[S]],
+    ) -> None:
+        self.f2 = f2
+        super().__init__(stream_a, stream_b, output_stream_group)
+
+    def set_input_a(self, stream_handle_a: StreamHandle[T]) -> None:
+        self.input_stream_handle_a = stream_handle_a
+        self.integrated_stream_a = Integrate(self.input_stream_handle_a)
+        self.delayed_integrated_stream_a = Delay(self.integrated_stream_a.output_handle())
+
+    def set_input_b(self, stream_handle_b: StreamHandle[R]) -> None:
+        self.input_stream_handle_b = stream_handle_b
+        self.integrated_stream_b = Integrate(self.input_stream_handle_b)
+        self.delayed_integrated_stream_b = Delay(self.integrated_stream_b.output_handle())
+
+        self.f2_a_b = Lifted2(self.input_stream_handle_a, self.input_stream_handle_b, self.f2, None)
+        self.f2_a_delayed_integrated_b = Lifted2(
+            self.input_stream_handle_a, self.delayed_integrated_stream_b.output_handle(), self.f2, None
+        )
+        self.f2_delayed_integrated_a_b = Lifted2(
+            self.delayed_integrated_stream_a.output_handle(), self.input_stream_handle_b, self.f2, None
+        )
+
+    def step(self) -> bool:
+        self.integrated_stream_a.step()
+        self.delayed_integrated_stream_a.step()
+
+        self.integrated_stream_b.step()
+        self.delayed_integrated_stream_b.step()
+
+        self.f2_a_b.step()
+        self.f2_a_delayed_integrated_b.step()
+        self.f2_delayed_integrated_a_b.step()
+
+        ab = self.f2_a_b.output().latest()
+        adib = self.f2_a_delayed_integrated_b.output().latest()
+        diab = self.f2_delayed_integrated_a_b.output().latest()
+
+        group = self.output().group()
+        self.output().send(group.add(group.add(ab, adib), diab))
+
+        return True
+
+
 def step_n_times[T](operator: Operator[T], n: int) -> None:
     for _ in range(n):
         operator.step()
